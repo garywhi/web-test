9a38cce1d49075e582fcd1bffef3281dab56ae0d
garywhi <50818672+garywhi@users.noreply.github.com>
2023-03-11 00:59:12 -1000

    Update App.js
    Changed BrowserRouter to hashRouter to fix 404 refresh issue with gh-pages.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 
 function App() {
   return (
-    <BrowserRouter basename={window.location.pathname || ''}>
+    <HashRouter>
       <Routes>
         <Route exact path='/' element={<Home />}></Route>
         <Route path='/projects' exact element={<Projects />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
