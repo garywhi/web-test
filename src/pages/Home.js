c0bd6f8fef81e83d3e0f8ab8f35f1e9e58f843f1
garywhi <50818672+garywhi@users.noreply.github.com>
2023-03-16 23:55:58 -1000

    Commented out hero section
    Hero section no longer in use.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,13 @@ export default function Home() {
   return (
     <>
     <Navbar />
-    <Hero
+    {/*<Hero
       cName='hero'
       heroImg='https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80'
       title='Gary Wong'
       position='Software Engineer'
       text=''
-       />
+  />*/}
     <About />
     </>
     
